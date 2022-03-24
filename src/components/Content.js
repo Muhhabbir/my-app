487dbb116d40bd9efc327a2f827fa61dd5be5188
Muhhabbir <101520159+Muhhabbir@users.noreply.github.com>
2022-03-24 17:28:23 +0700

    Add files via upload

---
+++ src/components/Content.js
@@ -0,0 +1,50 @@
+const Content = () => {
+    return (
+        <div className="playlist-container">
+            <div className="playlist-item">
+                <img
+                    className="album-cover"
+                    src="https://upload.wikimedia.org/wikipedia/en/4/45/SECOND_CHANCE_NOAH_COVER_ALBUM.jpg"
+                    alt="cover"
+                />
+                <div className="playlist-content">
+                    <h2 className="playlist-artist">Noah</h2>
+                    <h3 className="playlist-song">Hero-Second Chance</h3>
+                </div>
+                <div className="playlist-action">
+                    <button className="playlist-button">Select</button>
+                </div>
+            </div>
+            <div className="playlist-item">
+                <img
+                    className="album-cover"
+                    src="https://upload.wikimedia.org/wikipedia/en/4/45/SECOND_CHANCE_NOAH_COVER_ALBUM.jpg"
+                    alt="cover"
+                />
+                <div className="playlist-content">
+                    <h2 className="playlist-artist">Noah</h2>
+                    <h3 className="playlist-song">Hero-Second Chance</h3>
+                </div>
+                <div className="playlist-action">
+                    <button className="playlist-button">Select</button>
+                </div>
+            </div>
+            <div className="playlist-item">
+                <img
+                    className="album-cover"
+                    src="https://upload.wikimedia.org/wikipedia/en/4/45/SECOND_CHANCE_NOAH_COVER_ALBUM.jpg"
+                    alt="cover"
+                />
+                <div className="playlist-content">
+                    <h2 className="playlist-artist">Noah</h2>
+                    <h3 className="playlist-song">Hero-Second Chance</h3>
+                </div>
+                <div className="playlist-action">
+                    <button className="playlist-button">Select</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Content;
